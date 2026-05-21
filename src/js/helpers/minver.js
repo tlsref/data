e4ef553a1ca8e7615e9966ceb02539479cb5e8bc
Glenn Strauss <gstrauss@gluelogic.com>
2026-05-21 11:12:47 -0400

    delete non-static resources from original ssl-config-generator repo

--- src/js/helpers/minver.js
+++
@@ -1,8 +0,0 @@
-import normalizever from './normalizever.js';
-const semverLte = require('semver/functions/lte');
-
-export default (minimumver, curver) => {
-  const min = normalizever(minimumver);
-  const ver = normalizever(curver);
-  return semverLte(min, ver, { includePrerelease: true, loose: true });
-};
